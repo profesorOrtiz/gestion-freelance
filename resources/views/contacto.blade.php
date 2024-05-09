8bce169d1a02af454a9c5944fe303ebbc6c895f6
NotebookAcer\Personal <juankortiz@proton.me>
2024-05-09 16:33:38 -0300

    Creación del componente para layout general

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<x-layout>
     <x-titulo>Contacto</x-titulo>
 
     <p>Esta es la página para contactarte con {{ $nombre }}</p>
     <a href="/">Volver al inicio</a>
 
     <x-footer />
-</body>
-</html>
+</x-layout>
