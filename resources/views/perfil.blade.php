6a80e40582bfec7d5734e87863a71120c77fdf5f
DESKTOP-CASA\Pesonal <juankortiz@proton.me>
2024-05-13 17:21:53 -0300

    Modificar el layout principal utilizando TailwindCSS

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Perfil</title>
-</head>
-<body>
+<x-layout>
     <x-titulo>Este es el perfil de {{ $nombre }}</x-titulo>
 
     <h2>Mis proyectos</h2>
@@ -34,5 +26,4 @@
     &copy; AÑO ACTUAL SU NOMBRE - Todos los derechos reservados.
         --}}
     <x-footer />
-</body>
-</html>
+</x-layout>
