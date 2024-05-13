13638912f88c59f0662fc189790149603fdf507d
DESKTOP-CASA\Pesonal <juankortiz@proton.me>
2024-05-13 16:09:09 -0300

    Definir código CSS y Javascript en el componente

--- resources/views/components/titulo.blade.php
+++ resources/views/components/titulo.blade.php
@@ -1,10 +1,28 @@
 <style>
-    .titulo {
+    h1 {
+        /* Selector de etiqueta */
+    }
+    .titulo { /* Selector de clases */
         color: navy;
         font-weight: 600;
         font-size: 38px;
     }
+    #titulo-secccion {
+        /* Selector de ID */
+    }
 </style>
-<h1 class="titulo">
+<h1 class="titulo" id="titulo-seccion">
     {{ $slot }}
 </h1>
+<script>
+    // Seleccionar el elemento
+    const titulo = document.querySelector("#titulo-seccion");
+    // Agregar un evento al elemento
+    titulo.addEventListener("click", function() {
+        // Definir la lógica del método que responde al evento
+        // Muestra un popup en la página.
+        alert("Hiciste click en el titulo");
+        // Muestra el mensaje en la consola del navegador
+        console.log("Hiciste click en el titulo");
+    });
+</script>
