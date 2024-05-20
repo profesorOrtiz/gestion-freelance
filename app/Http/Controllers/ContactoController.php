5bc7dfe77a06ec4d4aa76a8a89a05e25fb413202
NotebookAcer\Personal <juankortiz@proton.me>
2024-05-20 17:33:05 -0300

    Definir Form Request

--- app/Http/Controllers/ContactoController.php
+++ app/Http/Controllers/ContactoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContactoGuardarRequest;
 use Illuminate\Http\Request;
 
 class ContactoController extends Controller
@@ -14,7 +15,13 @@ class ContactoController extends Controller
         ]);
     }
 
-    public function guardar(Request $request) {
-        dd($request);
+    public function guardar(ContactoGuardarRequest $request) {
+        // 1) Validar -> automaticamente el ContactoGuardarRequest
+
+        // 2) Procesar: guardar en la BD -> Proximamente
+
+        // 3) Informar el resultado del proceso
+        // Redirigiendo a la página previa con una variable de estado cuyo valor es ok
+        return back()->with('estado', 'ok');
     }
 }
--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -3,17 +3,29 @@
 
     <p>Esta es la página para contactarte con {{ $nombre }}</p>
 
+    @if (session('estado') === 'ok'):
+        <div>
+            Hemos recibido tu mensaje y te contestaremos a la brevedad
+        </div>
+    @endif
+
     <form action="{{ route('contacto.guardar') }}" method="POST" class="my-10">
         @csrf
 
         <div>
             <label for="email">Email</label>
-            <input type="text" name="email" id="email">
+            <input type="text" name="email" id="email" value="{{ old('email') }}">
+            @error('email')
+                <p class="text-red-800">{{ $message }}</p>
+            @enderror
         </div>
 
         <div>
             <label for="mensaje">Mensaje</label>
-            <textarea name="mensaje" id="mensaje"></textarea>
+            <textarea name="mensaje" id="mensaje">{{ old('mensaje') }}</textarea>
+            @error('mensaje')
+                <p class="text-red-800">{{ $message }}</p>
+            @enderror
         </div>
 
         <div class="mt-5">
