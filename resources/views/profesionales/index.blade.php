ab0dd95c34d64927bce211fe861f4fe056e36fc4
DESKTOP-CASA\Pesonal <juankortiz@proton.me>
2024-09-09 17:17:03 -0300

    Revertir aplicación de estilos dinámicos para los tags de tecnologías

--- resources/views/profesionales/index.blade.php
+++ resources/views/profesionales/index.blade.php
@@ -6,18 +6,9 @@
             <h2 class="text-2xl mb-2">
                 {{ $profesional->nombre . ' ' . $profesional->apellido }}
             </h2>
-            @php
-                $colores = [
-                    'violet' => ['bg-violet-300', 'text-violet-900', 'hover:bg-violet-400'],
-                    'green' => ['bg-green-300', 'text-green-900', 'hover:bg-green-400'],
-                    'yellow' => ['bg-yellow-300', 'text-yellow-900', 'hover:bg-yellow-400'],
-                    'red' => ['bg-red-300', 'text-red-900', 'hover:bg-red-400'],
-                    'blue' => ['bg-blue-300', 'text-blue-900', 'hover:bg-blue-400'],
-                ];
-            @endphp
             {{-- tecnologias es el método de relación que existe en el modelo Profesional --}}
             @foreach ($profesional->tecnologias as $tecnologia)
-                <a href="{{ route('tecnologias.show', $tecnologia->id) }}" class="inline-block {{ $colores[$tecnologia->color][0] }} {{ $colores[$tecnologia->color][1] }} py-1 px-3 rounded-xl mr-2 {{ $colores[$tecnologia->color][2] }}">
+                <a href="{{ route('tecnologias.show', $tecnologia->id) }}" class="inline-block bg-violet-300 text-violet-900 py-1 px-3 rounded-xl mr-2 hover:bg-violet-400">
                     {{ $tecnologia->nombre }}
                 </a>
             @endforeach
